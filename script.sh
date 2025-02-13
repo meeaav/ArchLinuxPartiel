4fa5e4426bf85196703ebe1fc974ee25d012f237
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2025-02-13 20:48:31 +0100

    ok

--- script.sh
+++ script.sh
@@ -5,9 +5,19 @@ timedatectl set-timezone Europe/Paris
 
 #Crééer une table de partition avec sfdisk, et ses partitions
 sfdisk /dev/sda << EOF
-1, 512M
-2, 10G,
+1, 500M
 ;
 EOF
 
-#Chiffrer avec LVM la partition
+#Chiffrer avec LVM le disque
+pvcreate /dev/sda2
+vgcreate vg0 /dev/sda2
+
+lvcreate -L 2G -n lv_swap vg0
+lvcreate -L 20G -n lv_VM vg0
+lvcreate -L 5G -n lv_tmp vg0
+lvcreate -L 10G -n lv_home_father vg0
+lvcreate -L 10G -n lv_home_son vg0
+lvcreate -L 5G -n lv_bin vg0
+lvcreate -L 10G -n lv_fathersecret vg0
+lvcreate -l 100%FREE -n lv_root vg0

d6e20b8763e3df4d0cc19c7b13f52a063baf0898
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2025-02-13 20:29:31 +0100

    ok

--- script.sh
+++ script.sh
@@ -3,8 +3,11 @@
 #Fix de l'horloge système
 timedatectl set-timezone Europe/Paris
 
-#Crééer une table de partition avec sfdisk
+#Crééer une table de partition avec sfdisk, et ses partitions
 sfdisk /dev/sda << EOF
 1, 512M, 
+2, 10G,
 ;
 EOF
+
+#Chiffrer avec LVM la partition

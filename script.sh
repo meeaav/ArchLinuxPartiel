7cfd1a7dd2a1c4c0c7b63275b6eae1bd28faddaa
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2025-02-13 21:58:12 +0100

    ok

--- script.sh
+++ script.sh
@@ -1,56 +1,61 @@
 #!/bin/bash
 
 #Fix de l'horloge système
-#timedatectl set-timezone Europe/Paris
+timedatectl set-timezone Europe/Paris
 
 #Crééer une table de partition avec sfdisk, et ses partitions
-#sfdisk /dev/sda << EOF
-#1, 500M
-#;
-#EOF
+sfdisk /dev/sda << EOF
+1, 500M
+;
+EOF
 
 #Coombo chiffrement LUKS et LVM sur /dev/sda2
-#read -s -p "Entrez le mot de passe LUKS : " password
-#echo -n "$password" | cryptsetup luksOpen /dev/sda2 crypt
-#echo -n "$password" | cryptsetup luksFormat /dev/sda2
+read -s -p "Entrez le mot de passe LUKS : " password
+echo -n "$password" | cryptsetup luksOpen /dev/sda2 crypt
+echo -n "$password" | cryptsetup luksFormat /dev/sda2
 
 #Création des volumes logiques avec LVM
-#pvcreate /dev/mapper/crypt
-#vgcreate vg0 /dev/mapper/crypt
+pvcreate /dev/mapper/crypt
+vgcreate vg0 /dev/mapper/crypt
 
-#lvcreate -L 2G -n lv_swap vg0
-#lvcreate -L 20G -n lv_VM vg0
-#lvcreate -L 5G -n lv_tmp vg0
-#lvcreate -L 10G -n lv_home_father vg0
-#lvcreate -L 10G -n lv_home_son vg0
-#lvcreate -L 5G -n lv_bin vg0
-#lvcreate -L 10G -n lv_fathersecret vg0
-#lvcreate -l 100%FREE -n lv_root vg0
+lvcreate -L 2G -n lv_swap vg0
+lvcreate -L 20G -n lv_VM vg0
+lvcreate -L 5G -n lv_tmp vg0
+lvcreate -L 10G -n lv_home_father vg0
+lvcreate -L 10G -n lv_home_son vg0
+lvcreate -L 5G -n lv_bin vg0
+lvcreate -L 10G -n lv_fathersecret vg0
+lvcreate -L 5G -n lv_share vg0
+lvcreate -l 100%FREE -n lv_root vg0
 
 #Formatage des partitions
-#mkfs.ext4 /dev/mapper/vg0-lv_root
-#mkfs.ext4 /dev/mapper/vg0-lv_home_father
-#mkfs.ext4 /dev/mapper/vg0-lv_home_son
-#mkfs.ext4 /dev/mapper/vg0-lv_bin
-#mkfs.ext4 /dev/mapper/vg0-lv_fathersecret
-#mkfs.ext4 /dev/mapper/vg0-lv_tmp
-#mkfs.ext4 /dev/mapper/vg0-lv_VM
-#mkswap /dev/mapper/vg0-lv_swap
+mkfs.ext4 /dev/mapper/vg0-lv_root
+mkfs.ext4 /dev/mapper/vg0-lv_home_father
+mkfs.ext4 /dev/mapper/vg0-lv_home_son
+mkfs.ext4 /dev/mapper/vg0-lv_bin
+mkfs.ext4 /dev/mapper/vg0-lv_fathersecret
+mkfs.ext4 /dev/mapper/vg0-lv_tmp
+mkfs.ext4 /dev/mapper/vg0-lv_VM
+mkfs.ext4 /dev/mapper/vg0-lv_share
+mkswap /dev/mapper/vg0-lv_swap
 
 #Montage des partitions
-#mount /dev/mapper/vg0-lv_root /mnt
-#mkdir /mnt/home
-#mount /dev/mapper/vg0-lv_home_father /mnt/home/father
-#mkdir /mnt/home/son
-#mount /dev/mapper/vg0-lv_home_son /mnt/home/son
-#mkdir /mnt/bin
-#mount /dev/mapper/vg0-lv_bin /mnt/bin
-#mkdir /mnt/fathersecret
-#mount /dev/mapper/vg0-lv_fathersecret /mnt/fathersecret
-#mkdir /mnt/tmp
-#mount /dev/mapper/vg0-lv_tmp /mnt/tmp
-#mkdir -p /mnt/var/VM
-#mount /dev/mapper/vg0-lv_VM /mnt/var/VM
+mount /dev/mapper/vg0-lv_root /mnt
+mkdir /mnt/home
+mount /dev/mapper/vg0-lv_home_father /mnt/home/father
+mkdir /mnt/home/son
+mount /dev/mapper/vg0-lv_home_son /mnt/home/son
+mkdir /mnt/bin
+mount /dev/mapper/vg0-lv_bin /mnt/bin
+mkdir /mnt/fathersecret
+mount /dev/mapper/vg0-lv_fathersecret /mnt/fathersecret
+mkdir /mnt/tmp
+mount /dev/mapper/vg0-lv_tmp /mnt/tmp
+mkdir -p /mnt/var/VM
+mount /dev/mapper/vg0-lv_VM /mnt/var/VM
+mkdir /mnt/share
+mount /dev/mapper/vg0-lv_share /mnt/share
+swapon /dev/mapper/vg0-lv_swap
 
 #Création des répertoires
 mkfs.ext4 /dev/sda1
@@ -61,6 +66,3 @@ else
     mkdir /mnt/boot
 fi
 mount /dev/sda1 /mnt/boot
-
-
-

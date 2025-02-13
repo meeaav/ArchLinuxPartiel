6f5e67f6785137ea46c7f1de03ef79b4b0fffa69
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2025-02-13 21:09:14 +0100

    ok

--- script.sh
+++ script.sh
@@ -9,19 +9,19 @@ sfdisk /dev/sda << EOF
 ;
 EOF
 
- #Coombo chiffrement LUKS et LVM
+ #Coombo chiffrement LUKS et LVM sur /dev/sda2
 cryptsetup luksFormat /dev/sda2
 cryptsetup open /dev/sda2 crypt
 
 #Création des volumes logiques avec LVM
-pvcreate /dev/sda2
-vgcreate vg0 /dev/sda2
+#pvcreate /dev/sda2
+#vgcreate vg0 /dev/sda2
 
-lvcreate -L 2G -n lv_swap vg0
-lvcreate -L 20G -n lv_VM vg0
-lvcreate -L 5G -n lv_tmp vg0
-lvcreate -L 10G -n lv_home_father vg0
-lvcreate -L 10G -n lv_home_son vg0
-lvcreate -L 5G -n lv_bin vg0
-lvcreate -L 10G -n lv_fathersecret vg0
-lvcreate -l 100%FREE -n lv_root vg0
+#lvcreate -L 2G -n lv_swap vg0
+#lvcreate -L 20G -n lv_VM vg0
+#lvcreate -L 5G -n lv_tmp vg0
+#lvcreate -L 10G -n lv_home_father vg0
+#lvcreate -L 10G -n lv_home_son vg0
+#lvcreate -L 5G -n lv_bin vg0
+#lvcreate -L 10G -n lv_fathersecret vg0
+#lvcreate -l 100%FREE -n lv_root vg0

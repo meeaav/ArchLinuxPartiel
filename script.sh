437277c3549ad8b1ce0415835917ae97f1963460
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2025-02-13 21:27:58 +0100

    ok

--- script.sh
+++ script.sh
@@ -15,8 +15,8 @@
 #echo -n $password | cryptsetup luksOpen /dev/sda2 crypt
 
 #Création des volumes logiques avec LVM
-pvcreate /dev/sda2/crypt
-vgcreate vg0 /dev/sda2/crypt
+pvcreate /dev/mapper/crypt
+vgcreate vg0 /dev/mapper/crypt
 
 lvcreate -L 2G -n lv_swap vg0
 lvcreate -L 20G -n lv_VM vg0

32692b3020f45b4cd474a79e8ac0643cdac9912e
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2025-02-13 23:52:20 +0100

    ok

--- script.sh
+++ script.sh
@@ -91,8 +91,9 @@ crypt2=$(blkid -s UUID -o value /dev/sda2)
 arch-chroot /mnt << EOF
 
 echo 'GRUB_ENABLE_CRYPTODISK=y' >> /etc/default/grub
+echo 'GRUB_CMDLINE_LINUX="cryptdevice=UUID=$crypt2:crypt root=/dev/mapper/vg0-lv_root" >> /etc/default/grub
+
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
-echo 'GRUB_CMDLINE_LINUX="cryptdevice=UUID=$crypt2:crypt root=/dev/mapper/vg0-lv_root" >> /etc/default/grub
-EOF
 
+EOF

676e7697c9a79393c489d4ca1952f66f6bd609fc
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2025-02-13 21:34:13 +0100

    ok

--- script.sh
+++ script.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 #Fix de l'horloge système
-#timedatectl set-timezone Europe/Paris
+timedatectl set-timezone Europe/Paris
 
 #Crééer une table de partition avec sfdisk, et ses partitions
-#sfdisk /dev/sda << EOF
-#1, 500M
-#;
-#EOF
+sfdisk /dev/sda << EOF
+1, 500M
+;
+EOF
 
 #Coombo chiffrement LUKS et LVM sur /dev/sda2
-#password="password"
-#echo -n $password | cryptsetup luksFormat /dev/sda2
-#echo -n $password | cryptsetup luksOpen /dev/sda2 crypt
+password="password"
+echo -n $password | cryptsetup luksFormat /dev/sda2
+echo -n $password | cryptsetup luksOpen /dev/sda2 crypt
 
 #Création des volumes logiques avec LVM
-#pvcreate /dev/mapper/crypt
-#vgcreate vg0 /dev/mapper/crypt
+pvcreate /dev/mapper/crypt#
+vgcreate vg0 /dev/mapper/crypt
 
-#lvcreate -L 2G -n lv_swap vg0
-#lvcreate -L 20G -n lv_VM vg0
-#lvcreate -L 5G -n lv_tmp vg0
-#lvcreate -L 10G -n lv_home_father vg0
-#lvcreate -L 10G -n lv_home_son vg0
-#lvcreate -L 5G -n lv_bin vg0
-#lvcreate -L 10G -n lv_fathersecret vg0
-#lvcreate -l 100%FREE -n lv_root vg0
+lvcreate -L 2G -n lv_swap vg0
+lvcreate -L 20G -n lv_VM vg0
+lvcreate -L 5G -n lv_tmp vg0
+lvcreate -L 10G -n lv_home_father vg0
+lvcreate -L 10G -n lv_home_son vg0
+lvcreate -L 5G -n lv_bin vg0
+lvcreate -L 10G -n lv_fathersecret vg0
+lvcreate -l 100%FREE -n lv_root vg0
 
 #Formatage des partitions
 mkfs.ext4 /dev/mapper/vg0-lv_root

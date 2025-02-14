9cdda9060b7d58676148e09e6c895f56a1923815
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2025-02-14 23:01:45 +0100

    ok

--- tmp.sh
+++ tmp.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 arch-chroot /mnt << EOF
-pacman -S --noconfirm virtualbox-guest-utils xf86-video-vmware open-vm-tools xf86-input-vmmouse xf86-input-vboxmouse xf86-input-vboxvideo xf86-video-vmware xf86-video-vboxvideo xf86-video-vesa 
-EOF
+pacman -S --noconfirm virtualbox-guest-utils xf86-video-vmware open-vm-tools

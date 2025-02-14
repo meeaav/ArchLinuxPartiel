e00dd5b69e3648dabc87a1dd9b9cd490ef7a0589
Kyllian LEFEVRE <kyllian.lefevre@icloud.com>
2025-02-14 21:39:44 +0100

    ok

--- kyllian.sh
+++ kyllian.sh
@@ -90,8 +90,55 @@ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
 
-# Installation de GNOME (optionnel)
-#arch-chroot /mnt << EOF
-#pacman -S --noconfirm gnome gnome-extra
-#systemctl enable gdm
-#EOF
+
+#Création des users, avec leur home, et azerty1Z3 pour mdp, et le groupe pour dossier partagé
+arch-chroot /mnt << EOF
+useradd -d /home/father -m father -p azerty1Z3 -G share -s /bin/bash
+useradd -d /home/son -m son -p azerty1Z3 -G share -s /bin/bash
+EOF
+
+#Création du dossier partagé
+mkdir /mnt/share
+chgrp share /mnt/share
+chmod 770 /mnt/share
+
+#Création des dossiers pour les users
+mkdir /mnt/home/father/share
+chown father:share /mnt/home/father/share
+chmod 770 /mnt/home/father/share
+
+mkdir /mnt/home/son/share
+chown son:share /mnt/home/son/share
+chmod 770 /mnt/home/son/share
+
+#Création des arboréscences pour les users
+mkdir -p /mnt/home/father/{Documents,Images,Musique,Vidéos}
+mkdir -p /mnt/home/son/{Documents,Images,Musique,Vidéos}
+
+#Installation de vi pour le fils 
+arch-chroot /mnt << EOF
+pacman -S --noconfirm vi
+EOF
+
+#Installation des logiciels de base (firefox, calculatrice, libreoffice, brave, vlc, discord, steam..)
+arch-chroot /mnt << EOF
+pacman -S --noconfirm firefox gnome-calculator libreoffice-still brave vlc discord steam
+EOF
+
+#Installation de hyprland pour le père
+arch-chroot /mnt << EOF
+pacman -S --noconfirm hyprland
+EOF
+
+#Ajout d'une configuration pour le père
+
+
+
+#Installation de l'interface graphique gnome
+arch-chroot /mnt << EOF
+pacman -S --noconfirm gnome gnome-extra
+systemctl enable gdm
+EOF
+cryptsetup close crypt
+echo "Le système est prêt à être redémarré."
+reboot

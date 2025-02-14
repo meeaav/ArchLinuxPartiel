7b7ee721f826f16394b71bcac1b7781a31f4c728
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2025-02-14 22:42:42 +0100

    ok

--- test.sh
+++ test.sh
@@ -1,22 +1,6 @@
-#Création du dossier partagé
-mkdir /mnt/share
-chgrp share /mnt/share
-chmod 770 /mnt/share
-
-#Création des dossiers pour les users
-mkdir /mnt/home/father/share
-chown father:share /mnt/home/father/share
-chmod 770 /mnt/home/father/share
-
-mkdir /mnt/home/son/share
-chown son:share /mnt/home/son/share
-chmod 770 /mnt/home/son/share
 
 
-#Installation de vi pour le fils 
-arch-chroot /mnt << EOF
-pacman -S --noconfirm vi
-EOF
+
 
 #Installation des logiciels de base (firefox, calculatrice, libreoffice, brave, vlc, discord, steam..)
 arch-chroot /mnt << EOF
--- tmp.sh
+++ tmp.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-# Création du dossier partagé
-mkdir -p /mnt/share
-
-# Définir le groupe propriétaire et les permissions
-chgrp share /mnt/share
-chmod 2770 /mnt/share  # 2 pour le sticky bit, 770 pour les permissions
-
-# Création des liens symboliques dans les répertoires home des utilisateurs
-mkdir -p /home/father /home/son
-ln -sf /mnt/share /home/father/share
-ln -sf /mnt/share /home/son/share
-
-# Définir les propriétaires et les permissions pour les liens symboliques
-chown father:share /home/father/share
-chown son:share /home/son/share
+#Installation de vi pour le fils 
+arch-chroot /mnt << EOF
+pacman -S --noconfirm vi
+EOF

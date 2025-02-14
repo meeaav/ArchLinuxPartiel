4f4082da2fed63e9cc8b16fcc226b33af6f6a0aa
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2025-02-14 22:41:36 +0100

    ok

--- angel.sh
+++ angel.sh
@@ -111,5 +111,22 @@ EOF
 mkdir -p /mnt/home/father/{Documents,Images,Musique,Vidéos}
 mkdir -p /mnt/home/son/{Documents,Images,Musique,Vidéos}
 
+#Création du dossier partagé
+mkdir -p /mnt/share
+
+# Définir le groupe propriétaire et les permissions
+chgrp share /mnt/share
+chmod 2770 /mnt/share  # 2 pour le sticky bit, 770 pour les permissions
+
+# Création des liens symboliques dans les répertoires home des utilisateurs
+mkdir -p /home/father /home/son
+ln -sf /mnt/share /home/father/share
+ln -sf /mnt/share /home/son/share
+
+# Définir les propriétaires et les permissions pour les liens symboliques
+chown father:share /home/father/share
+chown son:share /home/son/share
+
+
 pacman tree -S -y
 echo "Le système est prêt à être redémarré."
--- tmp.sh
+++ tmp.sh
@@ -15,14 +15,3 @@ ln -sf /mnt/share /home/son/share
 # Définir les propriétaires et les permissions pour les liens symboliques
 chown father:share /home/father/share
 chown son:share /home/son/share
-
-# Monter la partition partagée (supposons que /dev/mapper/vg0-lv_share est la partition partagée)
-mount /dev/mapper/vg0-lv_share /mnt/share
-
-# Vérification des permissions
-ls -ld /mnt/share /home/father/share /home/son/share
-
-# Ajouter le montage automatique au démarrage dans /etc/fstab
-echo "/dev/mapper/vg0-lv_share /mnt/share ext4 defaults 0 2" >> /etc/fstab
-
-echo "Le dossier partagé est monté et configuré."

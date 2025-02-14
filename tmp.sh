b744e00629510cb2a8f5883236905ac48c894a0e
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2025-02-14 22:46:30 +0100

    ok

--- tmp.sh
+++ tmp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Installation des logiciels de base (firefox,libreoffice, brave, vlc, discord, steam..)
+#Installation des logiciels de base 
 arch-chroot /mnt << EOF
-pacman -S --noconfirm firefox libreoffice-still vlc discord
+pacman -S --noconfirm firefox vlc discord libreoffice libreoffice-l10n-fr openclipart*-libreoffice hyphen-fr libreoffice-help-fr mythes-fr fonts-crosextra-caladea
 EOF

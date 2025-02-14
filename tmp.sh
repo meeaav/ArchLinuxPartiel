e31979f73f77e4c5dfcf28d17ae8c969e67bd51e
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2025-02-14 23:01:16 +0100

    ok

--- tmp.sh
+++ tmp.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-pacman -S virtualbox virtualbox-host-modules-arch virtualbox-guest-iso
+arch-chroot /mnt << EOF
+pacman -S --noconfirm virtualbox-guest-utils xf86-video-vmware open-vm-tools xf86-input-vmmouse xf86-input-vboxmouse xf86-input-vboxvideo xf86-video-vmware xf86-video-vboxvideo xf86-video-vesa 
+EOF

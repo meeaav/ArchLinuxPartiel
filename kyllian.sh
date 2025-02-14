37165027c03ac525d81c8a4b8469d7414083a767
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2025-02-14 20:03:24 +0100

    ok

--- kyllian.sh
+++ kyllian.sh
@@ -80,12 +80,14 @@ EOF
 
 
 crypt2=$(blkid -s UUID -o value /dev/sda2)
-echo "GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$crypt2:crypt root=/dev/mapper/vg0-lv_root\"" >> /mnt/etc/default/grub
+echo "GRUB_CMDLINE_LINUX="cryptdevice=/dev/sda2:crypt root=/dev/mapper/vg0-lv_root"" >> /mnt/etc/default/grub
+echo "GRUB_ENABLE_CRYPTODISK=y" >> /mnt/etc/default/grub
+echo "GRUB_PRELOAD_MODULES=\"luks cryptodisk lvm gcry_rijndael gcry_sha256\"" >> /mnt/etc/default/grub
 
 arch-chroot /mnt << EOF
 pacman -S --noconfirm cryptsetup
 sed -i 's/^HOOKS=.*/HOOKS=(base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck)/' /etc/mkinitcpio.conf
-echo 'GRUB_ENABLE_CRYPTODISK=y' >> /etc/default/grub
+
 mkinitcpio -P
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg

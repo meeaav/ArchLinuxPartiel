3bdd47ae923b99928a82d3c29e5d32e20892e95a
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2025-02-14 14:36:05 +0100

    ok

--- script.sh
+++ script.sh
@@ -66,29 +66,3 @@ else
 fi
 mount /dev/sda1 /mnt/boot/efi
 
-#Sync des miroirs
-reflector --country France --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-
-#Installation de la base
-pacstrap -K /mnt base linux linux-firmware lvm2 efibootmgr grub cryptsetup 
-
-genfstab -U /mnt >> /mnt/etc/fstab
-#Récupération de l'UID de la partition chiffrée
-arch-chroot /mnt << EOF
-echo "dm_crypt" >> /etc/modules-load.d/dm_crypt.conf
-EOF
-
-
-crypt2=$(blkid -s UUID -o value /dev/sda2)
-echo "GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$crypt2:crypt root=/dev/mapper/vg0-lv_root\"" >> /mnt/etc/default/grub
-
-arch-chroot /mnt << EOF
-pacman -S --noconfirm cryptsetup
-sed -i 's/^HOOKS=.*/HOOKS=(base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck)/' /etc/mkinitcpio.conf
-echo 'GRUB_ENABLE_CRYPTODISK=y' >> /etc/default/grub
-mkinitcpio -P
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-
-
-EOF

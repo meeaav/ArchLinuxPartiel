f96c75caefd47150bced8ffb0a42aa04f701bd22
Kyllian LEFEVRE <kyllian.lefevre@icloud.com>
2025-02-14 22:14:57 +0100

    ok

--- part2kyky.sh
+++ part2kyky.sh
@@ -1,5 +1,9 @@
 #Création des users, avec leur home, et azerty1Z3 pour mdp, et le groupe pour dossier partagé
 arch-chroot /mnt << EOF
+passwd << EOF
+azerty1Z3
+azerty1Z3
+EOF
 pacman -S hyprland --noconfirm
 pacman -S gnome --noconfirm
 systemctl start gdm.service

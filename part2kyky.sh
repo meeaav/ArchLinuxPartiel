c10d796c381da1119b20f250bb4b1d5379669553
Kyllian LEFEVRE <kyllian.lefevre@icloud.com>
2025-02-14 22:46:43 +0100

    ok

--- part2kyky.sh
+++ part2kyky.sh
@@ -1,99 +1,59 @@
-#Création des users, avec leur home, et azerty1Z3 pour mdp, et le groupe pour dossier partagé
-arch-chroot /mnt << EOF
-passwd << EOF
-esgi
-esgi
-EOF
-
-#Création des users, avec leur home, et azerty1Z3 pour mdp, et le groupe pour dossier partagé
 arch-chroot /mnt << EOF
 groupadd share
-
-# Générer des mots de passe chiffrés
 encrypted_esgi=$(openssl passwd -1 "esgi")
 encrypted_father=$(openssl passwd -1 "azerty123")
 encrypted_son=$(openssl passwd -1 "azerty1Z3")
-
-# Créer les utilisateurs avec des mots de passe chiffrés
 useradd esgi -p "$encrypted_esgi" -s /bin/bash -m
 useradd -d /home/father -m father -p "$encrypted_father" -G share -s /bin/bash
 useradd -d /home/son -m son -p "$encrypted_son" -G share -s /bin/bash
 EOF
 
-#Création des arboréscences pour les users
 mkdir -p /mnt/home/father/{Documents,Images,Musique,Vidéos}
 mkdir -p /mnt/home/son/{Documents,Images,Musique,Vidéos}
+
 arch-chroot /mnt << EOF
-pacman -S hyprland --noconfirm
-pacman -S --noconfirm sddm alacritty thunar firefox
-systemctl enable sddm
+pacman -S i3-wm i3status dmenu --noconfirm
+pacman -S --noconfirm lightdm lightdm-gtk-greeter
+pacman -S --noconfirm alacritty thunar firefox
+systemctl enable lightdm
 EOF
-mkdir -p /etc/sddm.conf.d
-arch-chroot /mnt << EOF
-mkdir -p /etc/sddm.conf.d
-cat > /etc/sddm.conf.d/hyprland.conf << EOL
-[Autologin]
-Session=hyprland.desktop
-User=father
-EOL
-EOF 
 
 arch-chroot /mnt << EOF
-mkdir -p /etc/hypr
-cat > /etc/hypr/hyprland.conf << EOL
-exec-once = dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-exec-once = waybar
-exec-once = nm-applet --indicator
-exec-once = swaybg -i /usr/share/backgrounds/archlinux/arch-wallpaper.jpg
-
-monitor=,preferred,auto,1
-
-input {
-    kb_layout = fr
-    follow_mouse = 1
-    sensitivity = 0
-}
-
-general {
-    gaps_in = 5
-    gaps_out = 10
-    border_size = 2
-    col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
-    col.inactive_border = rgba(595959aa)
-}
-
-decoration {
-    rounding = 10
-    blur = yes
-    blur_size = 3
-    blur_passes = 1
-}
-
-animations {
-    enabled = yes
-    bezier = myBezier, 0.05, 0.9, 0.1, 1.05
-    animation = windows, 1, 7, myBezier
-    animation = windowsOut, 1, 7, default, popin 80%
-    animation = border, 1, 10, default
-    animation = fade, 1, 7, default
-    animation = workspaces, 1, 6, default
-}
-
-dwindle {
-    pseudotile = yes
-    preserve_split = yes
-}
-
-master {
-    new_is_master = true
-}
-
-gestures {
-    workspace_swipe = off
-}
-
-misc {
-    disable_hyprland_logo = true
-}
+mkdir -p /etc/lightdm/lightdm.conf.d
+cat > /etc/lightdm/lightdm.conf.d/i3.conf << EOL
+[Seat:*]
+session-wrapper=/etc/lightdm/Xsession
+greeter-session=lightdm-gtk-greeter
+user-session=i3
 EOL
 EOF
+
+arch-chroot /mnt << EOF
+mkdir -p /etc/i3
+cat > /etc/i3/config << EOL
+set \$mod Mod4
+exec --no-startup-id nm-applet
+exec --no-startup-id pasystray
+exec --no-startup-id feh --bg-scale /usr/share/backgrounds/archlinux/arch-wallpaper.jpg
+bar {
+    status_command i3status
+}
+bindsym \$mod+Return exec alacritty
+bindsym \$mod+d exec dmenu_run
+bindsym \$mod+Shift+q kill
+bindsym \$mod+h split h
+bindsym \$mod+v split v
+bindsym \$mod+f fullscreen toggle
+bindsym \$mod+Shift+c reload
+bindsym \$mod+Shift+r restart
+bindsym \$mod+Shift+e exec "i3-nagbar -t warning -m 'Voulez-vous vraiment quitter i3 ?' -b 'Oui' 'i3-msg exit'"
+EOL
+EOF
+
+chmod 755 /mnt/home/father
+chmod 755 /mnt/home/son
+chmod 770 /mnt/share
+chmod 770 /mnt/home/father/share
+chmod 770 /mnt/home/son/share
+
+echo "Configuration terminée. Redémarrez pour démarrer i3."

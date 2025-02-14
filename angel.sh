1b901267df868439a6e58f764eb03eb461c505ad
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2025-02-14 20:27:57 +0100

    ok

--- angel.sh
+++ angel.sh
@@ -3,20 +3,27 @@
 # Fix de l'horloge système
 timedatectl set-timezone Europe/Paris
 loadkeys fr
+
+# Mise à jour du système
 pacman -Syu << EOF
 y
 EOF
 
-# Créer une table de partition avec sfdisk, et ses partitions
+# Création de la table de partition avec sfdisk
 sfdisk /dev/sda << EOF
-1, 500M
+, 500M, EF00  # Partition EFI (pour UEFI)
+, 500M, 8300  # Partition /boot (non chiffrée)
 ;
 EOF
 
-# Combo chiffrement LUKS et LVM sur /dev/sda2
+# Formater les partitions
+mkfs.vfat /dev/sda1  # Formater la partition EFI en vfat
+mkfs.ext4 /dev/sda2  # Formater la partition /boot en ext4
+
+# Chiffrement de la partition racine avec LUKS
 password="esgi"
-echo -e "$password\n$password" | cryptsetup luksFormat /dev/sda2
-echo -e "$password" | cryptsetup open /dev/sda2 crypt
+echo -e "$password\n$password" | cryptsetup luksFormat /dev/sda3
+echo -e "$password" | cryptsetup open /dev/sda3 crypt
 
 # Création des volumes logiques avec LVM
 pvcreate /dev/mapper/crypt
@@ -32,7 +39,6 @@ lvcreate -L 5G -n lv_share vg0
 lvcreate -l 100%FREE -n lv_root vg0
 
 # Formatage des partitions
-mkfs.vfat /dev/sda1
 mkfs.ext4 /dev/mapper/vg0-lv_root
 mkfs.ext4 /dev/mapper/vg0-lv_home_father
 mkfs.ext4 /dev/mapper/vg0-lv_home_son
@@ -44,13 +50,16 @@ mkswap /dev/mapper/vg0-lv_swap
 
 # Montage des partitions
 mount /dev/mapper/vg0-lv_root /mnt
+mkdir /mnt/boot
+mount /dev/sda2 /mnt/boot  # Monter /boot
+mkdir /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi  # Monter /boot/efi (UEFI)
+
 mkdir /mnt/home
 mkdir /mnt/home/father
 mount /dev/mapper/vg0-lv_home_father /mnt/home/father
 mkdir /mnt/home/son
 mount /dev/mapper/vg0-lv_home_son /mnt/home/son
-#mkdir /mnt/fathersecret
-#mount /dev/mapper/vg0-lv_fathersecret /mnt/fathersecret
 mkdir /mnt/tmp
 mount /dev/mapper/vg0-lv_tmp /mnt/tmp
 mkdir -p /mnt/var/VM
@@ -59,35 +68,40 @@ mkdir /mnt/share
 mount /dev/mapper/vg0-lv_share /mnt/share
 swapon /dev/mapper/vg0-lv_swap
 
-# Monter /dev/sda1 sur /mnt/boot
-if [ -d /mnt/boot/efi ]; then
-    echo "Le répertoire /mnt/boot/efi existe déjà."
-else
-    mkdir -p /mnt/boot/efi
-fi
-mount /dev/sda1 /mnt/boot/efi
-
-# Sync des miroirs
+# Synchronisation des miroirs
 reflector --country France --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
 # Installation de la base
-pacstrap -K /mnt base linux linux-firmware lvm2 efibootmgr grub cryptsetup 
+pacstrap -K /mnt base linux linux-firmware lvm2 efibootmgr grub cryptsetup
 
+# Génération du fichier fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Récupération de l'UID de la partition chiffrée
+# Configuration du chroot
 arch-chroot /mnt << EOF
 echo "dm_crypt" >> /etc/modules-load.d/dm_crypt.conf
-EOF
 
-crypt2=$(blkid -s UUID -o value /dev/sda2)
-echo "GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$crypt2:crypt root=/dev/mapper/vg0-lv_root\"" >> /mnt/etc/default/grub
+# Récupération de l'UUID de la partition chiffrée
+crypt2=$(blkid -s UUID -o value /dev/sda3)
+echo "GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$crypt2:crypt root=/dev/mapper/vg0-lv_root\"" >> /etc/default/grub
 
-arch-chroot /mnt << EOF
-pacman -S --noconfirm cryptsetup
+# Configuration de mkinitcpio
 sed -i 's/^HOOKS=.*/HOOKS=(base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck)/' /etc/mkinitcpio.conf
+
+# Configuration de GRUB pour le boot chiffré
 echo 'GRUB_ENABLE_CRYPTODISK=y' >> /etc/default/grub
+
+# Régénération de l'initramfs
 mkinitcpio -P
+
+# Installation de GRUB
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
+
+# Définir un mot de passe root
+echo "Définir un mot de passe pour l'utilisateur root :"
+passwd
 EOF
+
+# Fin du script
+echo "Installation terminée. Redémarrez le système avec 'reboot'."

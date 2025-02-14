eb934958539afcacad3dff6a09a9a39795fe9fd8
Kyllian LEFEVRE <kyllian.lefevre@icloud.com>
2025-02-14 23:27:21 +0100

    ok

--- kyllian.sh
+++ kyllian.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-#Fix de l'horloge système
+# Fix de l'horloge système
 timedatectl set-timezone Europe/Paris
 loadkeys fr
 pacman -Syu << EOF
 y
 EOF
 
-#Créer une table de partition avec sfdisk
+# Créer une table de partition avec sfdisk
 sfdisk /dev/sda << EOF
 1, 500M
 ;
 EOF
 
-#Chiffrement LUKS et LVM sur /dev/sda2
-password="azerty123"
+# Chiffrement LUKS et LVM sur /dev/sda2
+password="esgi"
 echo -e "$password\n$password" | cryptsetup luksFormat /dev/sda2
 echo -e "$password" | cryptsetup open /dev/sda2 crypt
 
-#Création des volumes logiques avec LVM
+# Création des volumes logiques avec LVM
 pvcreate /dev/mapper/crypt
 vgcreate vg0 /dev/mapper/crypt
 
@@ -31,7 +31,7 @@ lvcreate -L 10G -n lv_fathersecret vg0
 lvcreate -L 5G -n lv_share vg0
 lvcreate -l 100%FREE -n lv_root vg0
 
-#Formatage des partitions
+# Formatage des partitions
 mkfs.vfat /dev/sda1
 mkfs.ext4 /dev/mapper/vg0-lv_root
 mkfs.ext4 /dev/mapper/vg0-lv_home_father
@@ -42,7 +42,7 @@ mkfs.ext4 /dev/mapper/vg0-lv_VM
 mkfs.ext4 /dev/mapper/vg0-lv_share
 mkswap /dev/mapper/vg0-lv_swap
 
-#Montage des partitions
+# Montage des partitions
 mount /dev/mapper/vg0-lv_root /mnt
 mkdir -p /mnt/home/father
 mount /dev/mapper/vg0-lv_home_father /mnt/home/father
@@ -56,35 +56,35 @@ mkdir -p /mnt/share
 mount /dev/mapper/vg0-lv_share /mnt/share
 swapon /dev/mapper/vg0-lv_swap
 
-#Monter /dev/sda1 sur /boot (en dehors de /mnt pour pas qu'il soit chiffré)
+# Monter /dev/sda1 sur /boot (en dehors de /mnt)
 mkdir -p /boot
 mount /dev/sda1 /boot
 
-#Lier /boot à /mnt/boot avec un bind mount
+# Lier /boot à /mnt/boot avec un bind mount
 mkdir -p /mnt/boot
 mount --bind /boot /mnt/boot
 
-#Synchronisation des miroirs pour pacman
+# Synchronisation des miroirs
 reflector --country France --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
-#Installation de la base Linux
+# Installation de la base
 pacstrap -K /mnt base linux linux-firmware lvm2
 
-#Génération du fichier fstab, servant à monter les partitions au démarrage
+# Génération du fichier fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-#Configuration de GRUB
+# Configuration de GRUB
 crypt2=$(blkid -s UUID -o value /dev/sda2)
 echo "GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$crypt2:crypt root=/dev/mapper/vg0-lv_root\"" >> /mnt/etc/default/grub
 echo 'GRUB_ENABLE_CRYPTODISK=y' >> /mnt/etc/default/grub
 
-#Ajouter les hooks encrypt et lvm2 dans /etc/mkinitcpio.conf
+# Ajouter les hooks encrypt et lvm2 dans /etc/mkinitcpio.conf
 arch-chroot /mnt sed -i 's/^HOOKS=(.*)/HOOKS=(base udev autodetect modconf kms keyboard keymap consolefont block encrypt lvm2 filesystems fsck)/' /etc/mkinitcpio.conf
 
-#Régénérer l'initramfs pour prendre en compte les modifications
+# Régénérer l'initramfs
 arch-chroot /mnt mkinitcpio -P
 
-#Installation de GRUB et configuration 
+# Installation de GRUB et configuration
 arch-chroot /mnt << EOF
 pacman -S --noconfirm grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
@@ -134,6 +134,7 @@ EOF
 
 #Installation des logiciels de base 
 arch-chroot /mnt << EOF
+
 pacman -S --noconfirm firefox vlc discord libreoffice-still thunar
 pacman -S --noconfirm virtualbox-guest-utils xf86-video-vmware open-vm-tools
 pacman -S i3-wm i3status dmenu --noconfirm
